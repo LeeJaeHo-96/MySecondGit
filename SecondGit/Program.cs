1268b10400c41f87876b6c7d9efa0871ed3b3051
LeeJaeHo-96 <towpam123@gmail.com>
2024-07-18 15:53:16 +0900

    몬스터 추가
    몬스터 추가하였다

---
+++ SecondGit/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SecondGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("몬스터 추가");
+
+            Console.WriteLine(args);
+
+        }
+    }
+}
